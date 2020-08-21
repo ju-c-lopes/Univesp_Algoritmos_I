c1828e7d77d4c3f382a92f1d8be4ab08e99b5d3f
Juliano <juliano.co.lopes@gmail.com>
2020-08-21 13:04:15 -0300

    Add exerc 2.9

---
+++ Semana3/exercicio2.9.py
@@ -0,0 +1,41 @@
+"""
+
+Problema Prático 2.9
+
+Qual é o tipo de objeto ao qual essas expressões são avaliadas?
+
+a) False + False
+
+    R:) -> saída retorna o inteiro 0
+
+b) 2 * 3**2.0
+
+    R:) saída retorna o float 18.0
+
+c) 4 // 2 + 4 % 2
+
+    R:) saída retorna o int 2
+
+d) 2 = 3 == 4 or 5 >= 5
+
+    R:) saída retorna booleano True
+
+"""
+
+# Letra a)
+
+print('Exercicio a)\nFalse + False\n', False + False)
+print('\n')
+
+# Letra b)
+
+print('Exercicio b)\n2 * 3 ** 2.0\n', 2 * 3 ** 2.0)
+print('\n')
+
+# Letra c)
+print('Exercicio c)\n4 // 2 + 4 % 2\n', 4 // 2 + 4 % 2)
+print('\n')
+
+# Letra d)
+
+print('Exercicio d)\n2 + 3 == 4 or 5 >= 5\n', 2 + 3 == 4 or 5 >= 5)
